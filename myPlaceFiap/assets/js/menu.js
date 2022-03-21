d3bcbce1747975599575db6bd5d43893af5b7944
Felipe Thiago C. C. Oliveira <98181566+felipethiago21@users.noreply.github.com>
2022-03-21 10:46:38 -0300

    Delete myPlaceFiap directory

--- myPlaceFiap/assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn');
-const navMenu = document.querySelector('.nav-menu');
-
-menuBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('mobile-menu');
-});
--- myPlaceFiap/assets/js/scroll.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    const navBar = document.querySelector('.nav-bar');
-    // console.log(window.scrollY);
-    navBar.classList.toggle('active-scroll', window.scrollY >100);
-})
